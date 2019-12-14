3de0a3dce6a266317599e9063e24cfb54b284248
Jorge Luis FRias <jorgeluis.nos@gmail.com>
2019-12-14 18:25:13 -0400

    Add tableview sample to show the image

--- app/alloy.js
+++ app/alloy.js
@@ -10,7 +10,6 @@
 //
 // Alloy.Globals.someGlobalFunction = function(){};
 
-
 // added during app creation. this will automatically login to
 // ACS for your application and then fire an event (see below)
 // when connected or errored. if you do not use ACS in your
@@ -31,49 +30,110 @@ if (Ti.Filesystem.isExternalStoragePresent()) {
         Alloy.Globals.inspectionDir = Ti.Filesystem.applicationDataDirectory;
     }
 }
-(function(){
+(function() {
 
 })();
-
-Alloy.Globals.saveVideo = function (callback){
+Alloy.Globals.counterVideos = 1;
+Alloy.Globals.saveVideo = function(callback) {
     require("/permissions").checkCameraPermission(function() {
-        var nextVideoId = Math.floor(Math.random() * 100); 
+        //var nextVideoId = Math.floor(Math.random() * 100);
+        var nextVideoId = Alloy.Globals.counterVideos;
         Ti.Media.showCamera({
-            success: function (e) {
+            success : function(e) {
+                Alloy.Globals.counterVideos = Alloy.Globals.counterVideos+1;
                 //callback(null, e);
                 Ti.API.info("e.media.length:" + e.media.length);
-                var tempFile = Ti.Filesystem.getFile(Alloy.Globals.inspectionDir + "/inspections/" + nextVideoId + '.mp4');
-                    tempFile.write(e.media);
-                
+                var tempFile = Ti.Filesystem.getFile(Alloy.Globals.inspectionDir, nextVideoId + '.mp4');
+                tempFile.write(e.media);
+
                 var videoPlayer = Ti.Media.createVideoPlayer({
                     //url : e.media.nativePath,
                     media : e.media,
                     scalingMode : Titanium.Media.VIDEO_SCALING_ASPECT_FIT,
                     autoplay : true
                 });
-                
+
                 function setBackgroundImage(thumbResponse, error) {
-                    //console.log('--> setBackgroundImage() - thumbResponse: ', thumbResponse, ', error: ', error);
+                    console.log('--> setBackgroundImage() - thumbResponse: ', thumbResponse, ', error: ', error);
                     if (error) {
                         alert('could not capture video');
                         return;
                     }
-                    callback(e);
-                    
+
+                    var thumbanilImage = thumbResponse.image;
+                    var thumbFile = Ti.Filesystem.getFile(Alloy.Globals.inspectionDir, nextVideoId + '.jpg');
+                    thumbFile.write(thumbanilImage);
+
+                    var imageView = Ti.UI.createView({
+                        width : "352",
+                        height : "288",
+                        backgroundImage : thumbFile.nativePath
+                    });
+
+                    var lbl = Titanium.UI.createLabel({
+                        text : " Video ",
+                        backgroundColor : "#fff",
+                        font : {
+                            fontSize : 20
+                        },
+                        color : "black",
+                        left : 5,
+                        top : 5
+                    });
+                    imageView.add(lbl);
+                    var eventCalled = 0;
+                    imageView.addEventListener('postlayout', function(e) {
+                        if (eventCalled == 0) {
+                            eventCalled++;
+                            Ti.API.info('Inside postlayout event');
+                            Ti.API.info('size : ' + imageView.size.width + ' x ' + imageView.size.height);
+                            var blob = imageView.toImage();
+
+                            Ti.API.info("JSON.stringify(blob):" + JSON.stringify(blob));
+                            Ti.API.info('blob.length : ' + blob.length);
+                            Ti.API.info('blob.apiName : ' + blob.apiName);
+                            var thumbFileNew = Ti.Filesystem.getFile(Alloy.Globals.inspectionDir, nextVideoId + '.jpg');
+                            thumbFileNew.write(blob);
+                            var videoFile = {
+                                imageFileName : thumbFileNew.getName(),
+                                thumb : thumbFileNew.getName(),
+                                video : tempFile.getName(),
+                                isVideo : true
+                            };
+
+                            callback(videoFile);
+                            window.close();
+                            window.remove(imageView);
+                            imageView = null;
+                            window = null;
+                            blob = null;
+                            thumbanilImage = null;
+                            thumbFile = null;
+                            thumbFileNew = null;
+                            blob = null;
+                            videoFile = null;
+                        }
+                    });
+                    var window = Titanium.UI.createWindow();
+                    window.add(imageView);
+                    window.open();
+
                 }
+
+
                 videoPlayer.requestThumbnailImagesAtTimes([0], Titanium.Media.VIDEO_TIME_OPTION_NEAREST_KEYFRAME, setBackgroundImage);
-                
+
             },
-            cancel: function (e) {
+            cancel : function(e) {
                 callback(e, null);
             },
-            error: function (e) {
+            error : function(e) {
                 callback(e, null);
             },
             mediaTypes : [Titanium.Media.MEDIA_TYPE_VIDEO],
             videoMaximumDuration : 30000,
             videoQuality : Titanium.Media.QUALITY_LOW
-            
+
         });
     });
 };
--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,12 +1,62 @@
+var inspectionDir = Alloy.Globals.inspectionDir;
+var maxRowHeight = 92;
+var thumbWidth = 90;
+function createTableRowForVideos(video_info, videoId) {
+    console.log('!!!', video_info);
+    var videoThumb = Titanium.Filesystem.getFile(inspectionDir, video_info.thumb);
+    var thumbHeight = (video_info.height / video_info.width) * thumbWidth;
+    var row = Titanium.UI.createTableViewRow({
+        className : 'caption',
+        height : maxRowHeight,
+        info : video_info
+    });
 
-function updateTableView(){
-    console.log('~~> updateTableView()');
+    row.add(Titanium.UI.createImageView({
+        image : video_info.thumb ? videoThumb.nativePath : '/images/videoPlaying.png', // this is a BLOB now: TODO: store on local file system ...
+        top : '5',
+        left : '5',
+        width : thumbWidth,
+        sourceType : 'PlayVideoRow'
+    }));
+    row.add(Titanium.UI.createLabel({
+        left : '105',
+        right : '50',
+        height : Ti.UI.SIZE,
+        sourceType : 'VideoRow',
+        text : 'Tap to Add Caption',
+        color : '#000',
+        font : {
+            fontSize : '15'
+        },
+    }));
+    
+    return row;
 };
+
+function updateTableView(vid){
+    console.log('~~> updateTableView()');
+    
+ 
+    var data = [];
+    imageSection = Titanium.UI.createTableViewSection({
+        headerTitle : "Media"
+    });
+    
+    var row = createTableRowForVideos(vid, 0);
+    imageSection.add(row);
+    
+    console.log( "imageSection.rowCount: ", imageSection.rowCount);
+    if (imageSection.rowCount > 0)
+        data.push(imageSection);
+
+    $.tableView.setData(data);
+    
+}
 
 function takeVideos() {
     Alloy.Globals.saveVideo(function(videoFile) {
         console.log('videoFile----> ', JSON.stringify(videoFile));
-        updateTableView();
+        updateTableView(videoFile);
     });
 }
 
